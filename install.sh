656ce472119d65f7d2cf4582746953894423fcbf
Vitali Voinski <18062353+voinskiv@users.noreply.github.com>
2024-05-05 04:17:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-alias config='git --git-dir=$HOME/.config/ --work-tree=$HOME'
-
 git clone --bare https://github.com/voinskiv/config.git ~/.config
-config checkout
-config config --local status.showUntrackedFiles no
-chflags hidden $(config ls-files)
+git --git-dir="$HOME"/.config/ --work-tree="$HOME" checkout
+git --git-dir="$HOME"/.config/ --work-tree="$HOME" config --local status.showUntrackedFiles no
+chflags hidden "$(config ls-files)"
