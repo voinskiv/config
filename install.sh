e3c4153466be904fbcb1019efa8b4b429a7392c5
Vitali Voinski <18062353+voinskiv@users.noreply.github.com>
2024-05-05 03:09:04 +0200

    Add installation script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+alias config='git --git-dir=$HOME/.config/ --work-tree=$HOME'
+
+git clone --bare https://github.com/voinskiv/config.git ~/.config
+config checkout
+config config --local status.showUntrackedFiles no
+chflags hidden $(config ls-files)
