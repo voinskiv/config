65058b3f5faa10612e931bec15e691b293a914c7
Vitali Voinski <18062353+voinskiv@users.noreply.github.com>
2024-05-05 09:53:58 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ config() {
   git --git-dir="${HOME}"/.config/ --work-tree="${HOME}" "$@"
 }
 
+# Install config
 git clone --bare https://github.com/voinskiv/config.git ~/.config
 config checkout
 config config --local status.showUntrackedFiles no
@@ -11,3 +12,12 @@ config config --local status.showUntrackedFiles no
 chflags hidden "$(config ls-files)"
 
 source ~/.bash_profile
+
+# Install Homebrew
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install Homebrew packages
+brew bundle install
+
+# Set macOS settings
+source ~/.macos
